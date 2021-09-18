45c189dbf66233e9ac1f4631a9f7082e218661a4
Dima <dmi15307891@yandex.ru>
2021-09-18 23:35:00 +0500

    section four carousel

--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,20 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         infinite: true,
         speed: 500,
-        slidesToShow: 1,
+        // slidesToShow: 1,
         centerMode: true,
         variableWidth: true,
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"</button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"</button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"</button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
       });
   });
