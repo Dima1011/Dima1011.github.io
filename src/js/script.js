05360f9b53b3a20ca4f25fae312a8e9c16dc8522
Dima <dmi15307891@yandex.ru>
2021-10-17 22:14:34 +0500

    a business trip 2

--- src/js/script.js
+++ src/js/script.js
@@ -53,4 +53,20 @@ $(document).ready(function(){
 
     toggleSlade('.catalog-item__link');
     toggleSlade('.catalog-item__back');
+
+    // Modals
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
 });
